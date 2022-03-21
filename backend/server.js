507eb1eedb129c4b9e82c346ab60d22074bb6de8
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2022-03-21 18:33:36 +1100

    modify server.js

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,9 @@ connectDB();
 app.use(express.json()); //api can accept raw json form
 app.use(express.urlencoded({ extended: false })); //api can accept urlEncoded form
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello' });
-});
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'Hello' });
+// });
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/recipes', require('./routes/recipeRoutes'));
