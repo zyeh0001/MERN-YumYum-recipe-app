652bdff1b80caaea35910e938485d67bd258fb82
zyeh0001 <69131379+zyeh0001@users.noreply.github.com>
2022-03-21 17:36:07 +1100

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 require('colors');
 require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -18,6 +19,21 @@ app.get('/', (req, res) => {
 
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/recipes', require('./routes/recipeRoutes'));
+
+//Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the YumYum Recipe finder' });
+  });
+}
 
 //Handle error
 app.use(errorHandler);
